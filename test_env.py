372a1737a9b08d78e7bbf0778088cc7f9c4fef34
Vanshikasri14 <vanshikasrivastava112005@gmail.com>
2025-10-04 21:49:45 +0530

    Delete all tracked files from main branch

--- test_env.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-# load variables from .env
-load_dotenv()
-
-print("✅ GEMINI API key present:", bool(os.getenv("GEMINI_API_KEY")))
-print("KAGGLE_USERNAME:", os.getenv("KAGGLE_USERNAME"))
-print("ARTIFACTS_DIR:", os.getenv("ARTIFACTS_DIR"))
